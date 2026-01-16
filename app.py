d042f356e6141e0c0457da40876eb724891d0db2
komiayi <90276840+komiayi@users.noreply.github.com>
2026-01-16 10:04:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ if submit_button:
                             display: flex;
                             width: 100%;
                             height: 20px;
-                            border-radius: 10px;
+                            border-radius: 5px;
                             overflow: hidden;
                             box-shadow: inset 0 2px 5px rgba(0,0,0,0.2);
                             margin: 20px 0;
